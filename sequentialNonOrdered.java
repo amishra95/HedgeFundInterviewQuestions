e2a5adc88de84dbb1f81fb0cc67df180ce645305
Avanish Mishra <avanish.neham@gmail.com>
2018-08-14 10:35:33 -0500

    Brian's Interview N(LogN) solution case!

---
+++ sequentialNonOrdered.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class sequentialNonOrdered {
+
+	public int func(int[] arr, int num) {
+		
+		int max = 0;
+		
+	int[] brian = new int[arr.length];
+	
+	Arrays.sort(arr);
+for(int i = 1; i < arr.length; i++) {
+	if(arr[i]- arr[i-1] > 1) {
+		brian[i] = arr[i];
+	}
+	
+	
+	
+}
+
+return 0;
+	}
+	
+	
+	 
+}
